2be74ae56f97eab594a97ceae319dab107720a52
Sean <sean.a.blackman@student.uncommonhs.org>
2018-05-03 22:17:41 +0000

    Set up Google Maps API

--- js/script.js
+++ js/script.js
@@ -2,10 +2,7 @@
 
 console.log('hi');
 
-$('h1').click(function() {
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
-});
+
 
 var endpoint = "https://api.openweathermap.org/data/2.5/weather?zip=11216,us&appid=3bb00f30e525b91a1deb9cbd20254379";
 $.ajax({
@@ -15,4 +12,12 @@ $.ajax({
     }
 });
 
-//function(console.log(response)){
+$.ajax({
+    url: endpoint,
+    success: function(response) {
+        $("body").append("<div>" + response.coord.lon + "</div>");
+    }
+
+});
+
+//$("#searchButton").
