00757ae96523d8ffe2b31a9b34f375e08f94b19a
Sean <sean.a.blackman@student.uncommonhs.org>
2018-05-17 21:35:58 +0000

    Worked on API

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ console.log('hi');
 
 
 var endpoint = "https://api.openweathermap.org/data/2.5/weather?zip=11216,us&appid=3bb00f30e525b91a1deb9cbd20254379";
+var endpoint = "https://api.opencagedata.com/geocode/v1/json?q=PLACENAME&key=YOUR-API-KEY"
 $.ajax({
     url: endpoint,
     success: function(response) {
@@ -21,3 +22,11 @@ $.ajax({
 });
 
 //$("#searchButton").
+$("#searchBar").click(function(){
+    var dab = $("#searchButton").val();
+    $("#bob").html(dab);
+    map.flyTo({
+        center: []
+    })
+    
+});
